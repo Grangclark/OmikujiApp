abd383103180a94b39d7db2c4aaea32a544c3ffd
Grangclark <96944049+Grangclark@users.noreply.github.com>
2025-01-13 11:52:51 +0900

    Initial Commit

---
+++ OmikujiApp/OmikujiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmikujiAppApp.swift
+//  OmikujiApp
+//
+//  Created by 長橋和敏 on 2025/01/13.
+//
+
+import SwiftUI
+
+@main
+struct OmikujiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
